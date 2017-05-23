b9dc37fb8a4971d420d9fa234c1359c07da7f806
leelic07 <330314702@qq.com>
2017-05-23 18:11:43 +0800

    yuwutong

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <NavBar></NavBar>
+</template>
+
+<script>
+  import NavBar from './components/NavBar.vue'
+  export default {
+    name: 'app',
+    data(){
+        return {
+
+        }
+    },
+    components:{
+      NavBar
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row nav">
+    <div class="col-xs-24">
+      <div class="col-xs-4">
+        <router-link class="col-xs-24 pull-left" to="/" ><div class="pull-left">狱务公开</div>管理平台</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      data() {
+        return {
+
+        }
+      }
+  }
+</script>
+
+<style type="text/less" scoped>
+  @white:#fff;
+
+  *{
+    border:1px solid #000;
+  }
+
+
+</style>
